8fb1252118ddb481aaa67f6cfff599de7f26b077
momoyasha <matheusc.oliveira@live.com>
2025-07-07 14:45:15 +0100

    add view for debugging

---
+++ back_end/api/views/debug_views.py
@@ -0,0 +1,13 @@
+from api.repository.plane_repository import PlaneRepository
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class DebugView(APIView):
+    """
+    View para chamar lógica em desenvolvimento.
+    Útil para debugging.
+    """
+    def get(self, request):
+        method_to_debug = PlaneRepository.create_plane()
+        return Response({"status": True }, status=status.HTTP_200_OK)
