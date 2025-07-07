c055f2a4ea55f07256fc6c86d6d6ce1254f6c014
momoyasha <matheusc.oliveira@live.com>
2025-07-07 11:45:50 +0100

    update plane model

--- back_end/api/models/plane.py
+++ back_end/api/models/plane.py
@@ -3,9 +3,10 @@ from django.utils import timezone
 
 class Plane (models.Model):
     # id = models.AutoField(primary_key=True)
+    serial_number = models.TextField(blank=False, null=False)
     model = models.TextField(blank=False, null=False)
     active = models.BooleanField(blank=False, null=False, default=False)
-    latitude = models.FloatField(blank=False, null=False)
-    longitude = models.FloatField(blank=False, null=False)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
     updated_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
