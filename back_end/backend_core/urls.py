73a502f2b316b1f8dce8b8cbe6796f1b837e658d
momoyasha <matheusc.oliveira@live.com>
2025-07-07 14:45:39 +0100

    add debugging endpoint

--- back_end/backend_core/urls.py
+++ back_end/backend_core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views.debug_views import DebugView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('debug/', DebugView.as_view())
 ]
