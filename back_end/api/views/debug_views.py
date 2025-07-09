764a4f198ebe35c5fcf4938e8e61452e66daf670
momoyasha <matheusc.oliveira@live.com>
2025-07-09 12:09:04 +0100

    remove auth check from debug view

--- back_end/api/views/debug_views.py
+++ back_end/api/views/debug_views.py
@@ -3,12 +3,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from api.business.plane_business import PlaneBusiness
+from rest_framework.permissions import AllowAny
 
 class DebugView(APIView):
     """
     View para chamar lógica em desenvolvimento.
     Útil para debugging.
     """
+    permission_classes = [AllowAny]
+
     def get(self, request):
         # plane = PlaneRepository.get_plane_by_id(id=1)
         method_to_debug = PlaneBusiness.advance_plane_random(plane_id=1)
